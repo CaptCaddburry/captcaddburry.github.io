513e352e2e24ad1ce5d7171187959958d586b06c
Jimmy Cadd <52796010+CaptCaddburry@users.noreply.github.com>
2025-01-17 10:48:59 -0500

    Add files via upload

---
+++ brewfile.sh
@@ -0,0 +1,28 @@
+# Homebrew Install Command
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Taps
+brew tap hashicorp/tap
+
+# Formulaes
+brew install hashicorp/tap/terraform
+brew install hashicorp/tap/packer
+brew install git
+brew install python
+brew install mas
+
+# Casks
+brew install --cask discord
+brew install --cask displaylink
+brew install --cask docker
+brew install --cask google-chrome
+brew install --cask microsoft-office
+brew install --cask notion
+brew install --cask slack
+brew install --cask spotify
+brew install --cask steam
+brew install --cask visual-studio-code
+
+# Mac Store Apps
+mas install 379693831 # Audible
+mas install 302584613 # Kindle
