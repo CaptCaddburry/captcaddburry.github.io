66b93484bbe7c549d91a1dfa787cfc1d16a6bd83
Jimmy Cadd <52796010+CaptCaddburry@users.noreply.github.com>
2025-01-21 11:59:49 -0500

    Create devops_brewfile.sh

---
+++ devops_brewfile.sh
@@ -0,0 +1,23 @@
+# Homebrew Install Command
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Taps
+brew tap hashicorp/tap
+
+# Formulaes
+brew install act
+brew install aws-iam-authenticator
+brew install awscli
+brew install hashicorp/tap/packer
+brew install hashicorp/tap/terraform
+brew install helm
+brew install kubectl
+brew install k9s
+brew install node
+brew install rain
+brew install tfenv
+
+# Casks
+brew install --cask docker
+brew install --cask xquartz
