86ec2cdaf24c72622f5e1c4d99706b4d9dd8ee86
Jimmy Cadd <52796010+CaptCaddburry@users.noreply.github.com>
2025-01-21 13:25:03 -0500

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,18 @@
+# Graphical Apps
+winget install --id Amazon.Kindle
+winget install --id Discord.Discord
+winget install --id DisplayLink.GraphicsDriver.HotDesking
+winget install --id Docker.DockerDesktop
+winget install --id Google.Chrome
+winget install --id Microsoft.Office
+winget install --id Notion.Notion
+winget install --id SlackTechnologies.Slack
+winget install --id Spotify.Spotify
+winget install --id Valve.Steam
+winget install --id Microsoft.VisualStudioCode
+
+# CLI Tools
+winget install --id Git.Git
+winget install --id Hashicorp.Packer
+winget install --id Hashicorp.Terraform
+winget install --id Python.Python.3.13
