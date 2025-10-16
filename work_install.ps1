b31b0efbf55c9efd9b8abb251930f0d2fa5b9ab9
Jimmy Cadd <52796010+CaptCaddburry@users.noreply.github.com>
2025-10-16 12:19:40 -0400

    Update work_install.ps1
    Added in VS Code extensions as well

--- work_install.ps1
+++ work_install.ps1
@@ -1,13 +1,17 @@
 # CLI Tools
 $cli = @("Hashicorp.Packer", "Hashicorp.Terraform")
-
 foreach ($member in $cli) {
     winget install --id $member
 }
 
 # Graphical Tools
 $graphical = @("Docker.DockerDesktop", "Notion.Notion", "Spotify.Spotify", "Teamviewer.TeamViewer", "Microsoft.VisualStudioCode")
-
 foreach ($member in $graphical) {
     winget install --id $member
+}
+
+# VS Code Extensions
+$extensions = @("bladnman.auto-align", "ms-azuretools.vscode-containers", "jwsandeman.cyberpunk2077-theme", "ms-azuretools.vscode-docker", "docker.docker", "hashicorp.hcl", "hashicorp.terraform", "pkief.material-icon-theme", "ms-vscode.powershell", "ms-python.python")
+foreach ($member in $extensions) {
+    code --install-extension $member
 }
