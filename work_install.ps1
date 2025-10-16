ccd6596528ac66e367128a657dd8765cb66ed90f
Jimmy Cadd <52796010+CaptCaddburry@users.noreply.github.com>
2025-10-16 17:50:12 -0400

    Update work_install.ps1
    Using custom function to run install commands

--- work_install.ps1
+++ work_install.ps1
@@ -1,17 +1,21 @@
+# Install Commands
+$winget = "winget install --id "
+$code = "code --install-extension "
+
 # CLI Tools
 $cli = @("Hashicorp.Packer", "Hashicorp.Terraform")
-foreach ($member in $cli) {
-    winget install --id $member
-}
+ModInstall $winget $cli
 
 # Graphical Tools
 $graphical = @("Docker.DockerDesktop", "Notion.Notion", "Spotify.Spotify", "Teamviewer.TeamViewer", "Microsoft.VisualStudioCode")
-foreach ($member in $graphical) {
-    winget install --id $member
-}
+ModInstall $winget $graphical
 
 # VS Code Extensions
 $extensions = @("bladnman.auto-align", "ms-azuretools.vscode-containers", "jwsandeman.cyberpunk2077-theme", "ms-azuretools.vscode-docker", "docker.docker", "hashicorp.hcl", "hashicorp.terraform", "pkief.material-icon-theme", "ms-vscode.powershell", "ms-python.python")
-foreach ($member in $extensions) {
-    code --install-extension $member
+ModInstall $code $extensions
+
+function ModInstall([string]$function, [array]$apps) {
+    foreach ($member in $apps) {
+        Invoke-Expression ($function + $member)
+    }
 }
