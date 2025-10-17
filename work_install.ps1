d6db60ca5fe3b1685845c0d5d8f1869b562be3f4
Jimmy Cadd <52796010+CaptCaddburry@users.noreply.github.com>
2025-10-16 20:18:47 -0400

    Update work_install.ps1

--- work_install.ps1
+++ work_install.ps1
@@ -1,3 +1,10 @@
+# Custom Function to Run Installer Commands
+function ModInstall([string]$function, [array]$apps) {
+    foreach ($member in $apps) {
+        Invoke-Expression ($function + $member)
+    }
+}
+
 # Install Commands
 $winget = "winget install --id "
 $code = "code --install-extension "
@@ -13,9 +20,3 @@ ModInstall $winget $graphical
 # VS Code Extensions
 $extensions = @("bladnman.auto-align", "ms-azuretools.vscode-containers", "jwsandeman.cyberpunk2077-theme", "ms-azuretools.vscode-docker", "docker.docker", "hashicorp.hcl", "hashicorp.terraform", "pkief.material-icon-theme", "ms-vscode.powershell", "ms-python.python")
 ModInstall $code $extensions
-
-function ModInstall([string]$function, [array]$apps) {
-    foreach ($member in $apps) {
-        Invoke-Expression ($function + $member)
-    }
-}
